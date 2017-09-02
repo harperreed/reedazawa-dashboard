931a019d8b4bd3c2884dd80ead6a390091138a85
Harper Reed <harper@nata2.org>
2017-09-02 18:21:54 -0500

    updated to have batter

--- public/js/site.js
+++ public/js/site.js
@@ -1,33 +1,11 @@
  var getDeviceTelemetry = function () 
     {
         if (typeof okular === 'undefined' || okular === null) {
-            var battery = 90;
+            var battery = 0;
         }else {
             var s = okular.DevicesStatus()[okular.session_uuid]
-            if (typeof(okular)!=="undefined") {
-                $("#UUID").text(okular.session_uuid);
-                $("#RSSI").text(okular.RSSI);
-                $("#BATT").text(okular.BatteryLevel);
-                $("#TEMP").text(okular.Temperature);
-                
-                if (s.hasOwnProperty('FirmwareRevision'))  {
-                    $("#FW_version").text(s["FirmwareMajor"] + "."+ s["FirmwareMinor"] + "." + s["FirmwareRevision"]);
-                } else if (s.hasOwnProperty('ApplicationVersion')) {
-                    $("#FW_version").text(s["ApplicationVersion"]);
-                } else {
-                    $("#FW_version").text("No data.");
-                }
-
-            } else {
-                $("#UUID").text("unsupported");
-                $("#RSSI").text("unsupported");
-                $("#BATT").text("unsupported");
-                $("#TEMP").text("unsupported");
-                $("#FW_version").text("unsupported");
-            }
             var battery = okular.BatteryLevel
         }
-
 
         if (battery>90){
             $("#battery").html('<i class="fa fa-battery-full" aria-hidden="true"></i>')

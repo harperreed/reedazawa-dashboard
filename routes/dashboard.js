d703ba10c7b2ef99fdfa990a07660aff21530050
Harper Reed <harper@nata2.org>
2017-09-03 13:56:47 -0500

    greeting and an emoji test

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -132,7 +132,7 @@ router.get('/', function(req, res, next) {
             logs:logs,
             time:time,
             day: moment().format('dddd'),
-            hour: 22,//moment().format('k'),
+            hour: 5,//moment().format('k'),
             quotation:quotation
         }
         console.log(variables);

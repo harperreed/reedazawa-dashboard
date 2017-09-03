5a55a837bf372412cff745e220e3b25b773500ad
Harper Reed <harper@nata2.org>
2017-09-03 13:29:10 -0500

    added battery percentage

--- public/js/site.js
+++ public/js/site.js
@@ -6,18 +6,20 @@
             var s = okular.DevicesStatus()[okular.session_uuid]
             var battery = okular.BatteryLevel
         }
+        var battery_html = battery +"% "
 
         if (battery>90){
-            $("#battery").html('<i class="fa fa-battery-full" aria-hidden="true"></i>')
+            battery_html = battery_html + '<i class="fa fa-battery-full" aria-hidden="true"></i>'
         }else if (battery>75){
-            $("#battery").html('<i class="fa fa-battery-three-quarters" aria-hidden="true"></i>')
+            battery_html = battery_html + '<i class="fa fa-battery-three-quarters" aria-hidden="true"></i>'
         }else if (battery>50){
-            $("#battery").html('<i class="fa fa-battery-half" aria-hidden="true"></i>')
+            battery_html = battery_html +'<i class="fa fa-battery-half" aria-hidden="true"></i>'
         }else if (battery>25){
-            $("#battery").html('<i class="fa fa-battery-quarter" aria-hidden="true"></i>')
+            battery_html = battery_html +'<i class="fa fa-battery-quarter" aria-hidden="true"></i>'
         }else if (battery>=0){
-            $("#battery").html('<i class="fa fa-battery-empty" aria-hidden="true"></i>')
+            battery_html = battery_html +'<i class="fa fa-battery-empty" aria-hidden="true"></i>'
         }
+        $("#battery").html(battery_html)
     }
 
 

a40061ccb3fe3112063b629c3c13c4157ff6a856
Harper Reed <harper@nata2.org>
2017-09-04 12:42:51 -0500

    mabbe fixed

--- public/js/site.js
+++ public/js/site.js
@@ -75,7 +75,7 @@ var getWeather = function () {
 
 $(document).ready(function() {
     getDeviceTelemetry();
-    var update_time = 6000;
+    var update_time = 600000;
     var timer = setInterval(function() {
         getDeviceTelemetry();
         getQuotation();
